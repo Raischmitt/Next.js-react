7df89c3d7e6a036cbfaf7eac6b2c9cd2ae6fcb4f
Raíssa <raissajoaquim29@gmail.com>
2025-09-08 21:41:19 -0300

    adicionando banco de dados

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,26 @@
+import Link from "next/link";
+
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+
 export default function MealsPage() {
   return (
     <>
-      <h1>Meals</h1>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. it is easy and fun
+        </p>
+        <p classesName={classes.cta}>
+          <Link href="/meals/share">share your favorite recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
     </>
   );
 }
