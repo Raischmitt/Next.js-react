31e2db2445140f97cac010e4691e0f0274f679be
Raissa Schmitt <raissajoaquim29@gmail.com>
2025-09-10 19:14:43 -0300

    adição de page de erro

---
+++ app/meals/error.js
@@ -0,0 +1,12 @@
+'use client';
+
+export default function Error() {
+    return (
+
+        <main className="error">
+            <h1>An error occurred!</h1>
+            <p>failed to fetch meals data, please try again later</p>
+        </main>
+
+    );
+}
--- app/meals/page.js
+++ app/meals/page.js
@@ -1,8 +1,16 @@
 import Link from "next/link";
+import { Suspense } from 'react';
 
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
+
+async function Meals() {
+  const meals = await getMeals();
+
+  return <MealsGrid meals={meals} />
+}
+
 
 export default async function MealsPage() {
   const meals = await getMeals();
@@ -22,7 +30,10 @@ export default async function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense fallback={ <p className={classes.loading}>fetching meals...</p>}>
+          <Meals />
+        </Suspense>
+
       </main>
     </>
   );
