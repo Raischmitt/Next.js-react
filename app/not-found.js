1f6585f41e0257c65f8442d4920a137c1ad72915
Raissa Schmitt <raissajoaquim29@gmail.com>
2025-09-10 19:53:40 -0300

    erro de page n encontrada

--- app/not-found.js
+++ app/not-found.js
@@ -2,8 +2,8 @@ export default function NotFound() {
     return (
         <>
             <main className="not-found">
-                <h1>Not found</h1>
-                <p>Unfortunately, we could not find the request page or resource.</p>
+                <h1> Meal Not found</h1>
+                <p>Unfortunately, we could not find the request page meal data.</p>
             </main>
         </>
     )
