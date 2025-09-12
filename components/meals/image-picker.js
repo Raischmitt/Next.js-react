0f97b9122f988024e7e8872b9314f174e4e38724
Raíssa <raissajoaquim29@gmail.com>
2025-09-11 21:45:59 -0300

    mandando imagem para pasta publica

--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -49,7 +49,7 @@ export default function ImagePicker({ label, name }) {
             className={classes.input}
             type="file"
             id={name}
-            accept="image/png/, image/jpeg"
+            accept="image/png, image/jpeg"
             name={name}
             ref={imageInputRef}
             onChange={handleImageChange}
